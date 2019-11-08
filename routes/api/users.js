97992e0a0985aee8d5aac07a794f2b006608cb6b
caspianzx <caspianzx@gmail.com>
2019-11-08 11:17:56 +0800

    created user model and registration validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,34 @@
+// importing routing from express
 const express = require('express');
 const router = express.Router();
 
-//@router GET api/users  
-//@desc   TEST route
+//importing validation functions from express-validator using ES6 deconstruction assignment
+const {
+    check,
+    validationResult
+} = require('express-validator');
+
+//@router POST api/users  
+//@desc   POST route
 //access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more letters').isLength({
+            min: 6
+        })
+    ],
+    (req, res) => {
+        //response handling after validations
+        const errors = validationResult(req);
+        // if isEmpty is false return bad request
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+        console.log(req.body);
+        res.send('User route')
+    });
 
 
 
