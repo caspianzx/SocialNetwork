85c18e8588a9b109fc28ff4f8fec721665eaf34b
caspianzx <caspianzx@gmail.com>
2019-11-10 11:40:02 +0800

    created profile model

---
+++ models/Profile.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        //id from user model
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        // Array
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    githubusername: {
+        type: String
+    },
+    experience: [{
+        title: {
+            type: String,
+            required: true
+        },
+        company: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date,
+            required: true
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String,
+            required: true
+        },
+    }],
+    education: [{
+        school: {
+            type: String,
+            required: true
+        },
+        degree: {
+            type: String,
+            required: true
+        },
+        fieldofstudy: {
+            type: String,
+            required: true
+        },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date,
+            required: true
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String,
+            required: true
+        },
+    }],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    }
+});
+
+modules.exports = Profile = mongoose.model('profile', ProfileSchema);
