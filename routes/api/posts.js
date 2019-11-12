7e9f1a5bc20aa8f82b0dd3924ec9d68264f23ee6
caspianzx <caspianzx@gmail.com>
2019-11-12 13:05:56 +0800

    able to add post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
+//Import middleware
+const auth = require('../../middleware/auth');
+//
+//Import all models
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+//
+const {
+    check,
+    validationResult
+} = require('express-validator');
 
-//@router GET api/posts  
-//@desc   TEST route
-//access  Public
-router.get('/', (req, res) => res.send('Posts route'));
+
+//@router Post api/posts  
+//@desc   Create a post
+//access  Private
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    try {
+        //fetching user name and avatar from db
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save();
+
+        res.json(post)
+
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+
+    }
+
+});
 
 
 
