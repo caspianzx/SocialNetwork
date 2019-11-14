005e787396bad4e468e99b20092080629b89b80b
caspianzx <caspianzx@gmail.com>
2019-11-14 21:05:35 +0800

    created profile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/privateRoute'
+import CreateProfile from './components/profile-forms/CreateProfile';
+import PrivateRoute from './components/routing/privateRoute';
 import './App.css';
 // Redux
 import { Provider } from 'react-redux';
@@ -15,33 +16,34 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token);
 }
 
 const App = () => {
-  //similar to componentDidUpdate and but it will create a constant loop
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+	//similar to componentDidUpdate and but it will create a constant loop
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
 
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Route exact path='/' component={Landing} />
+					<section className='container'>
+						<Alert />
+						<Switch>
+							<Route exact path='/register' component={Register} />
+							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashboard} />
+							<PrivateRoute exact path='/create-profile' component={CreateProfile} />
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
 };
 
 export default App;
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,97 +1,106 @@
-import axios from "axios";
-import { setAlert } from "./alert";
+import axios from 'axios';
+import { setAlert } from './alert';
 import {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
-  USER_LOADED,
-  AUTH_ERROR,
-  LOGIN_FAIL,
-  LOGIN_SUCCESS
-} from "./types";
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_FAIL,
+	LOGIN_SUCCESS,
+	LOGOUT,
+	CLEAR_PROFILE
+} from './types';
 
-import setAuthToken from "../utils/setAuthToken";
+import setAuthToken from '../utils/setAuthToken';
 
 //LOAD USER
-export const loadUser = () => async dispatch => {
-  if (localStorage.token) {
-    setAuthToken(localStorage.token);
-  }
+export const loadUser = () => async (dispatch) => {
+	if (localStorage.token) {
+		setAuthToken(localStorage.token);
+	}
 
-  try {
-    const res = await axios.get("/api/auth");
-    dispatch({
-      type: USER_LOADED,
-      payload: res.data
-    });
-  } catch (err) {
-    dispatch({
-      type: AUTH_ERROR
-    });
-  }
+	try {
+		const res = await axios.get('/api/auth');
+		dispatch({
+			type: USER_LOADED,
+			payload: res.data
+		});
+	} catch (err) {
+		dispatch({
+			type: AUTH_ERROR
+		});
+	}
 };
 
 //REGISTER USER
-export const register = ({ name, email, password }) => async dispatch => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
+export const register = ({ name, email, password }) => async (dispatch) => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
 
-  const body = JSON.stringify({ name, email, password });
+	const body = JSON.stringify({ name, email, password });
 
-  try {
-    const res = await axios.post("/api/users", body, config);
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data
-    });
+	try {
+		const res = await axios.post('/api/users', body, config);
+		dispatch({
+			type: REGISTER_SUCCESS,
+			payload: res.data
+		});
 
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
 
-    if (errors) {
-      //errors is an array
-      //dispatch setAlert to indicate error
-      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
-    }
+		if (errors) {
+			//errors is an array
+			//dispatch setAlert to indicate error
+			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+		}
 
-    dispatch({
-      type: REGISTER_FAIL
-    });
-  }
+		dispatch({
+			type: REGISTER_FAIL
+		});
+	}
 };
 
 //Login USER
-export const login = (email, password) => async dispatch => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
+export const login = (email, password) => async (dispatch) => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
 
-  const body = JSON.stringify({ email, password });
+	const body = JSON.stringify({ email, password });
 
-  try {
-    const res = await axios.post("/api/auth", body, config);
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data
-    });
+	try {
+		const res = await axios.post('/api/auth', body, config);
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data
+		});
 
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
 
-    if (errors) {
-      //errors is an array
-      //dispatch setAlert to indicate error
-      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
-    }
+		if (errors) {
+			//errors is an array
+			//dispatch setAlert to indicate error
+			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+		}
 
-    dispatch({
-      type: LOGIN_FAIL
-    });
-  }
+		dispatch({
+			type: LOGIN_FAIL
+		});
+	}
+};
+
+//Logout / Clear Profile
+
+export const logout = () => (dispatch) => {
+	dispatch({ type: CLEAR_PROFILE });
+	dispatch({ type: LOGOUT });
 };
