4d5df2dbec94a97895704823910d3ca2b50475a8
caspianzx <caspianzx@gmail.com>
2019-11-14 17:22:39 +0800

    created profile reducer and get Current Profile

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = props => {
-  return (
-    <div>
-      <p>Dashboardfsfsjfhsjfhsjfhs</p>
-    </div>
-  );
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+	useEffect(() => {
+		getCurrentProfile();
+	}, []);
+	return (
+		<div>
+			<p>Dashboardfsfsjfhsjfhsjfhs</p>
+		</div>
+	);
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+	getCurrentProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+	profile: state.profile
+});
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
