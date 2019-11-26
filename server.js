a68803117d553f9d186743f26c670ae1ce1066fc
caspianzx <caspianzx@gmail.com>
2019-11-26 15:51:59 +0800

    edited server.js to production mode

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 //Connect to database
@@ -7,12 +8,11 @@ connectDB();
 
 //Init middleware(body parser is now included in express)
 
-app.use(express.json({
+app.use(
+  express.json({
     extended: false
-}));
-
-
-app.get('./', (req, res) => res.send('APP is running'));
+  })
+);
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -20,6 +20,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//Define PORT 
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+//Define PORT
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
