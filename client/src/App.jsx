6f8714a7c6ed97b9eac055db29d52ae8ed5e7ec8
caspianzx <caspianzx@gmail.com>
2019-11-26 14:04:46 +0800

    added post comment features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
-import Post from './components/posts/Post';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/privateRoute';
 import './App.css';
 // Redux
@@ -22,40 +23,57 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
-	//similar to componentDidUpdate and but it will create a constant loop
-	useEffect(() => {
-		store.dispatch(loadUser());
-	}, []);
+  //similar to componentDidUpdate and but it will create a constant loop
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
-	return (
-		<Provider store={store}>
-			<Router>
-				<Fragment>
-					<Navbar />
-					<Route exact path='/' component={Landing} />
-					<section className='container'>
-						<Alert />
-						<Switch>
-							<Route exact path='/register' component={Register} />
-							<Route exact path='/login' component={Login} />
-							<Route exact path='/profiles' component={Profiles} />
-							<Route exact path='/profile/:id' component={Profile} />
-							<PrivateRoute exact path='/dashboard' component={Dashboard} />
-							<PrivateRoute exact path='/create-profile' component={CreateProfile} />
-							<PrivateRoute exact path='/edit-profile' component={EditProfile} />
-							<PrivateRoute exact path='/add-experience' component={AddExperience} />
-							<PrivateRoute exact path='/add-education' component={AddEducation} />
-							<PrivateRoute exact path='/posts' component={Post} />
-						</Switch>
-					</section>
-				</Fragment>
-			</Router>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
+              />
+              <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,28 +1,63 @@
-import { GET_POST, POST_ERROR } from '../actions/types';
+import {
+  GET_POSTS,
+  GET_POST,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST
+} from '../actions/types';
 
 const initialState = {
-	posts: [],
-	post: null,
-	loading: true,
-	error: {}
+  posts: [],
+  post: null,
+  loading: true,
+  error: {}
 };
 
 export default function(state = initialState, action) {
-	const { type, payload } = action;
-	switch (type) {
-		case GET_POST:
-			return {
-				...state,
-				posts: payload,
-				loading: false
-			};
-		case POST_ERROR:
-			return {
-				...state,
-				error: payload,
-				loading: false
-			};
-		default:
-			return state;
-	}
+  const { type, payload } = action;
+  switch (type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: payload,
+        loading: false
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
+        loading: false
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false
+      };
+    //returning all posts except the one that matches payload(id)
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    //update likes only if user has liked the post before
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false
+      };
+    default:
+      return state;
+  }
 }
